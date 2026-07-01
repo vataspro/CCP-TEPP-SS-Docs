22b1e43519df3ec5114413871b2fa3a8b8b80157
Alexis Provatas <vataspro@gmail.com>
2026-07-01 16:24:22 +0100

    document least_squares.h

--- include/least_squares.h
+++ include/least_squares.h
@@ -4,14 +4,30 @@
 #ifndef LEAST_SQUARES_H
 #define LEAST_SQUARES_H
 
+/// @brief x, y datapoints for least squares regression
 typedef struct {
+    /// The input values
     double *x;
+    /// The output values
     double *y;
+    /// The number of datapoints
     int n;
 } Dataset;
 
+/// @brief Read in some parameter data to the Dataset struct
+/// @param filename Path to the file to read
+/// @return A populated Dataset with our data points
 Dataset read_data(const char *filename);
+
+/// @brief Solve the least squares method fitting the best line to fit a Dataset
+/// @param data Dataset to fit
+/// @param slope Variable that will hold the slope of the line
+/// @param intercept Variable that will hold the intercept of the line
 void solve_least_squares(const Dataset *data, double *slope, double *intercept);
+
+/// @brief Print the slope and intercept of a line
+/// @param slope Line slope
+/// @param intercept Line intercept
 void print_solution(double slope, double intercept);
 
 #endif // LEAST_SQUARES_H
